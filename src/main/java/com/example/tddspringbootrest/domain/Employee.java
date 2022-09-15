641dbe01c2654489e66f64574d4297b4b34c1d70
vivganes <vivek@vivekganesan.com>
2022-09-15 20:27:23 +0530

    09_passing_repository_test
    Co-authored-by: kirankashyap <kiran.kashyap.ds@gmail.com>

--- src/main/java/com/example/tddspringbootrest/domain/Employee.java
+++ src/main/java/com/example/tddspringbootrest/domain/Employee.java
@@ -1,11 +1,13 @@
 package com.example.tddspringbootrest.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Employee {
     @Id
+    @GeneratedValue
     private long id;
     private String name;
     private int age;
