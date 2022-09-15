2a81bbd1fded3de856b7b8be4c6e40aabc89cf5e
vivganes <vivek@vivekganesan.com>
2022-09-15 12:04:06 +0530

    00_preparing_the_ground
    Co-authored-by: kirankashyap <kiran.kashyap.ds@gmail.com>

---
+++ src/main/java/com/example/tddspringbootrest/TddSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddspringbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSpringBootRestApplication.class, args);
+	}
+
+}
